c189c7d75059eb4d2142b224735146ac427746fc
Ben Williams <8enwilliams@gmail.com>
2016-11-04 23:52:25 +1100

    c library included, checked in extconf

--- Rakefile
+++ Rakefile
@@ -3,10 +3,11 @@ require 'rspec/core/rake_task'
 require 'rake/extensiontask'
 
 RSpec::Core::RakeTask.new(:spec)
-task :default => :spec
 
 Rake::ExtensionTask.new('mpg123') do |ext|
   ext.lib_dir = 'lib/mpg123'
 end
 
 Rake::Task[:spec].prerequisites << :compile
+
+task :default => :spec
--- ext/mpg123/extconf.rb
+++ ext/mpg123/extconf.rb
@@ -1,3 +1,24 @@
 require 'mkmf'
 
-create_makefile('mpg123')
+LIB_NAME = 'mpg123'
+
+HEADER_DIRS = [
+  '/usr/local/include',         # homebrew / from source
+  RbConfig::CONFIG['includedir'], # ruby install locations
+  '/usr/include'                # fallback
+]
+
+LIB_DIRS = [
+  '/usr/local/lib',             # homebrew / from source
+  RbConfig::CONFIG['libdir'],     # ruby install locations
+  '/usr/lib'                    # fallback
+]
+
+dir_config(LIB_NAME, HEADER_DIRS, LIB_DIRS)
+
+unless find_header("#{LIB_NAME}.h") && 
+find_library(LIB_NAME, 'mpg123_decode')  
+  abort "lib#{LIB_NAME} not found"
+end
+
+create_makefile(LIB_NAME)
